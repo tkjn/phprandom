56d591385ab78d6f38ca90876da8977750496ea3
tkjn <tkjn@tkjngine.com>
2017-01-02 00:55:18 +0000

    Remove unnecessary handling of negative seeds

--- src/Integer/XorshiftStar.php
+++ src/Integer/XorshiftStar.php
@@ -20,35 +20,11 @@ class XorshiftStar implements SeededRandom
             ));
         }
 
-        // TODO: These can probably result in negative numbers, need to make sure the shifts and xor are consistent with unsigned ints
-        // Possible unsigned xor function here http://php.net/manual/en/language.operators.bitwise.php#58190
-        /*
-        function unsigned_xor32 ($a, $b)
-        {
-                $a1 = $a & 0x7FFF0000;
-                $a2 = $a & 0x0000FFFF;
-                $a3 = $a & 0x80000000;
-                $b1 = $b & 0x7FFF0000;
-                $b2 = $b & 0x0000FFFF;
-                $b3 = $b & 0x80000000;
-
-                $c = ($a3 != $b3) ? 0x80000000 : 0;
-
-                return (($a1 ^ $b1) |($a2 ^ $b2)) + $c;
-        }
-        */
-
         $this->seed ^= $this->seed >> 12;
         $this->seed ^= $this->seed << 25;
         $this->seed &= 0x7FFFFFFFFFFFFFFF;
         $this->seed ^= $this->seed >> 27;
-        return $min + bcmod(
-            bcmul(
-                bccomp($this->seed, '0'),
-                bcmul($this->seed, '2685821657736338717')
-            ),
-            $max - $min
-        );
+        return $min + bcmod(bcmul($this->seed, '2685821657736338717'), $max - $min);
     }
 
     public function seed(int $seed) : void
